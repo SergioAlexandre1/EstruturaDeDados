b3d6a03fb24d88123ed5308ce4eff746b52ea771
SergioAlexandre1 <159459950+SergioAlexandre1@users.noreply.github.com>
2025-08-11 17:09:34 -0300

    Add files via upload

---
+++ 6valores.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int A[6];
+    for(int i = 0; i < 6; i++){
+        scanf("%d", &A[i]);
+    }
+    for(int i = 0; i < 6; i++){
+        printf("%d", A[i]);
+    }
+}
